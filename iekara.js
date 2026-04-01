a60ab2a964f07e7c777affe91df9426ce39e1826
24067 <e1x24067@st.oit.ac.jp>
2026-04-01 14:35:49 +0900

    Update iekara.js

--- iekara.js
+++ iekara.js
@@ -1,6 +1,6 @@
 const timelst =[
     [399,430,459,532,579,654,714,774,834,894,954,1030,1043,1117,1168,1234],
-    [383,429,469,482,537,580,655,715,775,835,895,977,1021,1042,1110,1136,1166,1225,1285,1345,]
+    [383,429,469,482,537,580,655,715,775,835,885,977,1021,1042,1110,1136,1166,1225,1285,1345,]
 ];
 
 window.onload = function(){
@@ -8,10 +8,9 @@ window.onload = function(){
     let youbi = now.getDay();          //曜日
     let hour = now.getHours();         //時間
     let minute = now.getMinutes();     //分
-
+    let targetlst;
     let now_time = hour*60 + minute;   //今の時間を分に
     let found = 0;
-    let targetlst;
     if (youbi == 0 || youbi == 6){    //日曜(0)か土曜(6)ならリスト0を使用
         document.getElementById("doniti").style.display = "table";      //日曜(0)か土曜(6)なら土日の表を表示
         targetlst = timelst[0];       
@@ -64,8 +63,7 @@ function holiday(){
 
     let now_time = hour*60 + minute;   //今の時間を分に
     let found = 0;
-    let targetlst = timelst[typeIdx];
-    targetlst = timelst[0];
+    let targetlst = timelst[0];
 
     for (let i = 0; i < targetlst.length; i++){      //現在時刻の次のバスの時刻を表示
         let bus_time = targetlst[i];
